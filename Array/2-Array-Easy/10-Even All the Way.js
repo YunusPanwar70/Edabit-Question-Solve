c680fd20a85e3028890eea880cd76a5e30a7cfdf
yunus <yunuspanwar@gmail.com>
2024-02-01 15:53:09 +0530

    Solve some Buggy Question

---
+++ Array/2-Array-Easy/10-Even All the Way.js
@@ -0,0 +1,11 @@
+function getOnlyEvens(arr) {
+
+}
+
+console.log(getOnlyEvens([1, 3, 2, 6, 4, 8]));
+console.log(getOnlyEvens([0, 1, 2, 3, 4]));
+console.log(getOnlyEvens([1, 2, 3, 4, 5]));
+
+// ➞ [2, 4]
+// ➞ [0, 2, 4]
+// ➞ []
---
+++ Objects/Object.assign/2-Example.js
@@ -0,0 +1,5 @@
+function copy(obj) {
+    return Object.assign(obj)
+}
+
+console.log(copy({ a: 1 }));
